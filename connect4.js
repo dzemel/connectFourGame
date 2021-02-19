d2dfe9164aa8c3ed273789b37aa15a5c68fd839a
Jon K <Johnakao@gmail.com>
2021-02-18 20:24:47 -0800

    Fixed Bugs In Endgame Fn and Tie Game Condition

--- connect4.js
+++ connect4.js
@@ -98,6 +98,7 @@ function placeInTable(y, x) {
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -123,8 +124,8 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-  if (board.every((val) => val !== null)) {
-    endGame();
+  if (board[0].every(val => val !== null)) {
+    endGame('Tie Game');
   }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
@@ -145,7 +146,6 @@ function checkForWin() {
       ([y,x]) => y >= 0 && y < 6 && 
     x >= 0 && x < 7 && board[y][x] === currPlayer);
   }
-    // player
 
   // using HEIGHT and WIDTH, generate "check list" of coordinates
   // for 4 cells (starting here) for each of the different
